3d24f3aba0a6fcefa38fadb121f0623cecd35c7b
Gauthier Defrance <135462312+GauthierDefrance@users.noreply.github.com>
2025-09-08 16:02:12 +0200

    e

---
+++ TP1/Sorting.c
@@ -0,0 +1,5 @@
+//
+// Created by defra on 08/09/2025.
+//
+
+#include "Sorting.h"
---
+++ TP1/Sorting.h
@@ -0,0 +1,8 @@
+//
+// Created by defra on 08/09/2025.
+//
+
+#ifndef SORTING_H
+#define SORTING_H
+
+#endif //SORTING_H

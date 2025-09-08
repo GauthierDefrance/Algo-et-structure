40855f88cf89a7e77b13a4d380a61273df098deb
Gauthier Defrance <135462312+GauthierDefrance@users.noreply.github.com>
2025-09-08 16:20:23 +0200

    upd

--- TP1/Sorting.c
+++ TP1/Sorting.c
@@ -3,3 +3,33 @@
 //
 
 #include "Sorting.h"
+#include <stdio.h>
+
+typedef int Telement;
+
+
+/**
+*  Fonction qui permute deux Telement dans un  tab de Telement aux indices a et b.
+**/
+void permutation(Telement tab[], int n, int a, int b){
+  Telement tmp = tab[a];
+  tab[a] = tab[b];
+  tab[b] = tmp;
+}
+
+/**
+*  Fonction qui trouve l'élément minimum d'un tableau Telement, compris entre i et n.
+**/
+Telement findMin(Telement tab[], int n, int i){
+  Telement champion=tab[i];
+    for(int j=i; j<n; j++){
+      if(champion>tab[j]) champion=tab[j];
+    }
+  return champion;
+}
+
+
+
+
+
+
---
+++ TP1/main.c
@@ -0,0 +1,10 @@
+//
+// Created by defra on 08/09/2025.
+//
+#include <stdio.h>
+#include "Sorting.h"
+
+int main(){
+    Telement tab[]={1,2,3,4,5,6,7,8,9};
+    printf("%d",findMin(tab,9,0));
+}
